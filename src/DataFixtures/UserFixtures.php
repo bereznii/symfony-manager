b157fec738b164995252fdca47665cf2d36d4312
Dmytro <dima.berezniy.13@gmail.com>
2022-03-23 18:52:37 +0200

    feat: use fakerphp in fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -10,10 +10,13 @@ use App\Model\User\Entity\User\Id;
 use App\Model\User\Service\PasswordHasher;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private const LIMIT_USERS = 25;
+
     /**
      * @param PasswordHasher $hasher
      * @param UserPasswordHasherInterface $defaultPasswordHasher
@@ -29,6 +32,7 @@ class UserFixtures extends Fixture
      */
     public function load(ObjectManager $manager): void
     {
+        $faker = \Faker\Factory::create();
         $hash = $this->hasher->hash('password');
 
         $user = User::signUpByEmail(
@@ -39,12 +43,26 @@ class UserFixtures extends Fixture
             'token',
             new Name('John','Doe')
         );
-
         $user->confirmSignUp();
-
         $user->changeRole(Role::admin());
-
         $manager->persist($user);
+
+        for ($i = 0; $i < self::LIMIT_USERS; $i++) {
+            $user = User::signUpByEmail(
+                id: Id::next(),
+                created_at: new \DateTimeImmutable(),
+                email: new Email($faker->email()),
+                hash: $hash,
+                confirmToken: Uuid::uuid4()->toString(),
+                name: new Name(
+                    $faker->firstName(),
+                    $faker->lastName()
+                )
+            );
+
+            $user->confirmSignUp();
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
