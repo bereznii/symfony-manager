7bb86ecf2c51ffda715776f69324680a1e2902b5
Dmytro <dima.berezniy.13@gmail.com>
2022-03-23 22:38:39 +0200

    feat: use knp menu bundle for sidebar menu

---
+++ src/Twig/Menu/SidebarMenu.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SidebarMenu
+{
+    /**
+     * @param FactoryInterface $factory
+     * @param AuthorizationCheckerInterface $auth
+     */
+    public function __construct(
+        private FactoryInterface $factory,
+        private AuthorizationCheckerInterface $auth
+    ) {}
+
+    /**
+     * @return ItemInterface
+     */
+    public function build(): ItemInterface
+    {
+        $menu = $this->factory->createItem('root')
+            ->setChildrenAttributes([
+                'class' => 'nav nav-pills nav-sidebar flex-column',
+                'data-widget' => 'treeview',
+                'role' => 'menu',
+                'data-accordion' => 'false',
+            ]);
+
+        $menu->addChild('', ['route' => 'home'])
+            ->setExtra('icon', 'nav-icon fas fa-tachometer-alt')
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link')
+            ->setLabel('<p>Dashboard</p>')
+            ->setExtra('safe_label',true);
+
+        $menu->addChild('Work')->setAttribute('class', 'nav-header');
+
+        if ($this->auth->isGranted('ROLE_WORK_MANAGE_MEMBERS')) {
+            $menu->addChild('Membership', ['route' => 'work.members'])
+                ->setExtra('routes', [
+                    ['route' => 'work.members'],
+                    ['pattern' => '/^work\.members\..+/']
+                ])
+                ->setExtra('icon', 'nav-icon fas fa-users')
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link')
+                ->setLabel('<p>Membership</p>')
+                ->setExtra('safe_label',true);
+        }
+
+        $menu->addChild('Control')->setAttribute('class', 'nav-header');
+
+        if ($this->auth->isGranted('ROLE_MANAGE_USERS')) {
+            $menu->addChild('Users', ['route' => 'users'])
+                ->setExtra('icon', 'nav-icon fas fa-users-cog')
+                ->setExtra('routes', [
+                    ['route' => 'users'],
+                    ['pattern' => '/^users\..+/']
+                ])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link')
+                ->setLabel('<p>Users</p>')
+                ->setExtra('safe_label',true);
+        }
+
+        return $menu;
+    }
+}
